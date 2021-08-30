5b77917f5347b132017661f29d60158e64eef58c
Tim Davis <tsmancini@aol.com>
2021-08-30 11:18:55 -0400

    updated the movie list, chores and photos

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Favorite Pictures';
+  image1 = 'https://th.bing.com/th/id/OIP.x2KM-BswaqD_KkM94gE4dQAAAA?w=266&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7';
+  image2 = 'https://th.bing.com/th/id/OIP.EGzZLunxo0QQ6_GawS1mvwHaFG?w=270&h=186&c=7&r=0&o=5&dpr=1.5&pid=1.7';
+  image3 = 'https://th.bing.com/th/id/OIP.v-E4Afmv29tSsL9aapB1mgHaFj?w=248&h=186&c=7&r=0&o=5&dpr=1.5&pid=1.7';
 
   constructor() { }
 
